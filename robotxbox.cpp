50351755da3f2a352be4a51fff78c4e17765b982
lobitzb564 <56606893+lobitzb564@users.noreply.github.com>
2022-01-23 14:04:54 -0500

    Add files via upload

--- robotxbox.cpp
+++ robotxbox.cpp
@@ -1,37 +1,45 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
 
+#include <frc/Joystick.h>
 #include <frc/TimedRobot.h>
-#include <frc/XboxController.h>
-#include <frc/drive/DifferentialDrive.h>
+#include <frc/drive/MecanumDrive.h>
 #include <frc/motorcontrol/PWMSparkMax.h>
 
 /**
- * This is a demo program showing the use of the DifferentialDrive class.
- * Runs the motors with split arcade steering and an Xbox controller.
+ * This is a demo program showing how to use Mecanum control with the
+ * MecanumDrive class.
  */
 class Robot : public frc::TimedRobot {
-  frc::PWMSparkMax m_leftMotor{0};
-  frc::PWMSparkMax m_rightMotor{1};
-  frc::DifferentialDrive m_robotDrive{m_leftMotor, m_rightMotor};
-  frc::XboxController m_driverController{0};
-
  public:
   void RobotInit() override {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.SetInverted(true);
+    // Invert the right side motors. You may need to change or remove this to
+    // match your robot.
+    m_frontRight.SetInverted(true);
+    m_rearRight.SetInverted(true);
   }
 
   void TeleopPeriodic() override {
-    // Drive with split arcade style
-    // That means that the Y axis of the left stick moves forward
-    // and backward, and the X of the right stick turns left and right.
-    m_robotDrive.ArcadeDrive(-m_driverController.GetLeftY(),
-                             m_driverController.GetRightX());
+    /* Use the joystick X axis for lateral movement, Y axis for forward
+     * movement, and Z axis for rotation.
+     */
+    m_robotDrive.DriveCartesian(m_stick.GetX(), m_stick.GetY(), m_stick.GetZ());
   }
+
+ private:
+  static constexpr int kFrontLeftChannel = 0;
+  static constexpr int kRearLeftChannel = 1;
+  static constexpr int kFrontRightChannel = 2;
+  static constexpr int kRearRightChannel = 3;
+
+  static constexpr int kJoystickChannel = 0;
+
+  frc::PWMSparkMax m_frontLeft{kFrontLeftChannel};
+  frc::PWMSparkMax m_rearLeft{kRearLeftChannel};
+  frc::PWMSparkMax m_frontRight{kFrontRightChannel};
+  frc::PWMSparkMax m_rearRight{kRearRightChannel};
+  frc::MecanumDrive m_robotDrive{m_frontLeft, m_rearLeft, m_frontRight,
+                                 m_rearRight};
+
+  frc::Joystick m_stick{kJoystickChannel};
 };
 
 #ifndef RUNNING_FRC_TESTS
